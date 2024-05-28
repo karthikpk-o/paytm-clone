c1993b6ebcaab3d2fafd008668b8464920f5c382
Karthik P K <kpunichathayak@gmail.com>
2024-05-28 20:34:03 +0530

    added balance and transfer endpoint

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { authMiddleware } = require('../middleware');
 const { Account } = require('../db');
+const { default: mongoose } = require('mongoose');
 
 const router = express.Router();
 
@@ -14,7 +15,39 @@ router.get("/balance", authMiddleware, async(req,res)=>{
     })
 })
 
+router.post('/transfer', authMiddleware, async(req, res)=>{
+    const session = await mongoose.startSession();
 
+    session.startTransaction();
+    const {amount, to} = req.body;
+
+    const account = await Account.findOne({userId: req.userId}).session(session);
+    if(!account || account.balance < amount){
+        await session.abortTransaction();
+        res.status(400).json({
+            message: "Transaction failed"
+        })
+    }
+
+    const reciever = await Account.findOne({userId: to}).session(session)
+    if(!reciever){
+        await session.abortTransaction();
+        res.status(400).json({
+            message: "Transaction failed"
+        })
+    }
+
+    await Account.updateOne({userId: req.userId},{$inc: {balance: -amount}}).session(session);
+    await Account.updateOne({userId: to}, {$inc:{balance: amount}}).session(session);
+
+    await session.commitTransaction();
+    res.json({
+        message: "Transfer successful"
+    });
+
+
+
+})
 
 
 
