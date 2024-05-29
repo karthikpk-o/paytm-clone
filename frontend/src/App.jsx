4e5ab238111e9a89c573d55a0c8e5240502bba51
Karthik P K <kpunichathayak@gmail.com>
2024-05-29 19:45:48 +0530

    added routing to pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import {BrowserRouter} from "react-router-dom"
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/sigin" element={<Signin/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/send" element={<SendMoney/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
