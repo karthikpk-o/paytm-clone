a41231ddf3f5e2c405f48ea4c84314ad84e75637
Karthik P K <kpunichathayak@gmail.com>
2024-05-30 18:31:38 +0530

    styled signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import { Signup } from "./pages/Signup"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/sigin" element={<Signin/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/send" element={<SendMoney/>}/>
+        <Route path="/" element={<Signup/>}/>
       </Routes>
     </BrowserRouter>
   )
 }
 
 export default App
+
+/*
+<Route path="/sigin" element={<Signin/>}/>
+<Route path="/dashboard" element={<Dashboard/>}/>
+<Route path="/send" element={<SendMoney/>}/>
+*/
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,8 +2,12 @@
 
 export const Signup = ()=>{
     return (
-        <div className="">
-            hi
+        <div className="bg-slate-300 h-screen flex justify-center">
+            <div className="flex flex-col justify-center">
+                <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+
+                </div>
+            </div>
         </div>
     )
 }
